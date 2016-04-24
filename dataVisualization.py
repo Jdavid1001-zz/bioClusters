e8a38e1ddb049e262f32cce173f3228c5de84e3c
Juan David <juandominguez2017@u.northwestern.edu>
2016-04-24 00:42:11 -0500

    adds average line ontop of histograms

--- dataVisualization.py
+++ dataVisualization.py
@@ -10,7 +10,7 @@ def get_nominal(df, dataset):
     return df_nominal
 
 def get_standard(df, dataset):
-    df_dataset = df.loc[df_tr_in.dataset==dataset]
+    df_dataset = df.loc[df.dataset==dataset]
     df_standard = df_dataset.loc[df_dataset.trt==0]
     return df_standard
     
@@ -19,21 +19,35 @@ def hist_training(df_tr_in, datasets, listOfConditions):
     for dataset in datasets:
         df_standard = get_standard(df_tr_in, dataset)
         df_nominal = get_nominal(df_tr_in, dataset)
+        
         plt.figure()
         df_nominal['y'].plot.hist()
+        plt.axvline(df_nominal['y'].mean(), color='b', linestyle='dashed', linewidth=2)
         df_standard['y'].plot.hist().set_title(
             'nominal underneath; standard ontop ' + str(dataset))
+        plt.axvline(df_standard['y'].mean(), color='r', linestyle='dashed', linewidth=2)
+            
         plt.figure()
         df_nominal['y'].plot.hist().set_title('nominal ' + str(dataset))
-        print i
+        plt.axvline(df_nominal['y'].mean(), color='b', linestyle='dashed', linewidth=2)
         if listOfConditions[i] is not None:
             df_condition = df_tr_in.loc[listOfConditions[i][0]]
             df_condition_nominal = get_nominal(df_condition, dataset)
             df_condition_nominal['y'].plot.hist().set_title(
                 'nominal + conditional ' + str(dataset))
+            plt.axvline(df_condition_nominal['y'].mean(), color='r', linestyle='dashed', linewidth=2)
+                
+            plt.figure()
+            df_standard['y'].plot.hist()
+            plt.axvline(df_standard['y'].mean(), color='b', linestyle='dashed', linewidth=2)
+            df_condition_standard = get_standard(df_condition, dataset)
+            df_condition_standard['y'].plot.hist().set_title(
+                'standard + conditonal ' + str(dataset))
+            plt.axvline(df_condition_standard['y'].mean(), color='r', linestyle='dashed', linewidth=2)
         i = i + 1
 
 hist_training(df_tr_in, datasets,
                   [None, 
                        [df_tr_in.x29 < 51.3],
-                           [df_tr_in.x5 == 1] or [df_tr_in.x5 == 1], None])
+                           [df_tr_in.x5 == 1] or [df_tr_in.x5 == 1], 
+                                [df_tr_in.x4==1] or [df_tr_in.x4==2] and [df_tr_in.x22 > 57.7]])
