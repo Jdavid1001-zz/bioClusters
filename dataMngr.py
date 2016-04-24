52a98ae7da0052b1b650626b30dd6498c822a027
Juan David <juandominguez2017@u.northwestern.edu>
2016-04-23 21:55:36 -0500

    Adds ordering and index for input specific dataframe

--- dataMngr.py
+++ dataMngr.py
@@ -4,3 +4,7 @@ import pandas as pd
 def file2Dataframe(fileName):
     df = pd.read_csv(fileName, engine='c')
     return df
+    
+def order_and_index_training(df):
+    df = df.sort_values(columns=['dataset', 'trt', 'y'])
+    df = df.set_index(['dataset', 'trt'])
