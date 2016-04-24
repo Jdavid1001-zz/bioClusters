89d3dc72a0d30a800ec15e69040ad89e458672fe
Juan David <juandominguez2017@u.northwestern.edu>
2016-04-24 00:59:05 -0500

    refactored for consitency and legibilitye

--- dataVisualization.py
+++ dataVisualization.py
@@ -17,10 +17,13 @@ def get_standard(df, dataset):
 def plot_hists(df1, df2, dataset, title):
     plt.figure()
     df1['y'].plot.hist().set_title(title + str(dataset))
-    plt.axvline(df1['y'].mean(), color='b', linestyle='dashed', linewidth=2)
+    plt.axvline(df1['y'].mean(), color='b', 
+                linestyle='dashed', linewidth=2)
+                
     if df2 is not None:
         df2['y'].plot.hist()
-        plt.axvline(df2['y'].mean(), color='r', linestyle='dashed', linewidth=2)
+        plt.axvline(df2['y'].mean(), color='r', 
+                    linestyle='dashed', linewidth=2)
     
 def hist_training(df_tr_in, datasets, listOfConditions):
     i = 0
@@ -30,20 +33,26 @@ def hist_training(df_tr_in, datasets, listOfConditions):
         
         plot_hists(df_nominal, df_standard, dataset, 
                    'nominal underneath; standard ontop ')
+
         if listOfConditions[i] is None:
             plot_hists(df_nominal, None, dataset, 'nominal ')
+            
         else:
             df_condition = df_tr_in.loc[listOfConditions[i][0]]
             
             df_condition_nominal = get_nominal(df_condition, dataset)
-            plot_hists(df_nominal, df_condition_nominal, dataset, 'nominal + conditional ')
+            plot_hists(df_nominal, df_condition_nominal, dataset, 
+                       'nominal + conditional ')
             
             df_condition_standard = get_standard(df_condition, dataset)
-            plot_hists(df_standard, df_condition_standard, dataset, 'standard + conditonal ')
+            plot_hists(df_standard, df_condition_standard, dataset, 
+                       'standard + conditonal ')
+                       
         i = i + 1
 
 hist_training(df_tr_in, datasets,
                   [None, 
                        [df_tr_in.x29 < 51.3],
                            [df_tr_in.x5 == 1] or [df_tr_in.x5 == 1], 
-                                [df_tr_in.x4==1] or [df_tr_in.x4==2] and [df_tr_in.x22 > 57.7]])
+                                [df_tr_in.x4==1] or [df_tr_in.x4==2] 
+                                    and [df_tr_in.x22 > 57.7]])
