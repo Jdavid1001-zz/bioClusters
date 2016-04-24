d32d921448e70fac7b24fa12e1218d9f215f17bd
Juan David <juandominguez2017@u.northwestern.edu>
2016-04-23 21:56:40 -0500

    Returns a list of (indexed) datasets
    fixes bug in order_and_index_training

--- dataMngr.py
+++ dataMngr.py
@@ -8,3 +8,8 @@ def file2Dataframe(fileName):
 def order_and_index_training(df):
     df = df.sort_values(columns=['dataset', 'trt', 'y'])
     df = df.set_index(['dataset', 'trt'])
+    return df
+
+def get_dataset_list(df):
+    datasets = df['dataset'].unique().tolist()
+    return datasets
