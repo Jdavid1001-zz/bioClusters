109b3ba8a93ec22b0f57934ccbe6581b7dbd3a99
Juan David <juandominguez2017@u.northwestern.edu>
2016-04-23 20:47:45 -0500

    module to easily reference data path location

---
+++ data_paths.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+training_in_csv = 'csv_data/InnoCentive_9933623_Data.csv'
+training_out_csv = 'csv_data/InnoCentive_9933623_Training_Data_truth_subjects.csv'
+
+testing_in_csv = 'csv_data/InnoCentive_9933623_Data.csv'
